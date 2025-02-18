07f167fa9650626bf6f9d40a0c012c3f6b31052e
Belle Sangthong <belle.sangthong@iths.se>
2025-02-18 12:26:16 +0100

    Created a recursiveAdd method

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,9 @@ public class Main {
         } else if (operator.equals("minus")) {
             System.out.println(subtract(a, b));
 
+        } else if (operator.equals("recu")) {
+            System.out.println(recursiveAdd(a, b));
+
         } else {
             System.out.println("Invalid operator");
 
@@ -27,6 +30,13 @@ public class Main {
 
     public static int subtract(int a, int b) {
         return a - b;
+    }
+
+    public static int recursiveAdd(int a, int b) {
+        if (b <= 0) {
+            return a;
+        }
+        return recursiveAdd(a + 1, b - 1);
     }
 
 }
